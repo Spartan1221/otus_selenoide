38930cdf693ffe119cb2c9fe7eca5e85df11f568
mfilippov <mfilippov@it-one.ru>
2023-07-05 13:00:45 +0300

    fixed jenkins file

--- jenkins/ui-tests.groovy
+++ jenkins/ui-tests.groovy
@@ -1,37 +1,30 @@
 timeout(180) {
   node('gradle') {
-    timestamp {
+    timestamps {
       wrap([$class: 'BuildUser']) {
         summary = """|<b>Owner:</b> ${env.BUILD_USER}
                             |<b>Branch:</b> ${BRANCH}""".stripMargin()
         currentBuild.description = summary.replaceAll("\n", "<br>")
         owner_user_id = "${env.BUILD_USER_ID}"
       }
-      yaml_object = readYaml $YAML_CONFIG
-      for (key in params.keySet()) {
-        {
-          System.setProperty(key, params[key])
-        }
-      }
       stage('Checkout') {
-        checkout scv
+        checkout scm
       }K
       stage('Run tests') {
         tests_exit_code = sh(
             script: "gradle test -Dbrowser=$BROWSER_NAME -DbrowserVersion=$BROWSER_VERSION -DwebDriverRemoteUrl=$GRID_URL",
         )
-
         if (tests_exit_code != 0) {
-          currentBuild.status = 'UNSTABLE'
+          currentBuild.result = 'UNSTABLE'
         }
       }
       stage('Publish artifacts') {
         allure([
-            results: [{
-                path: 'target/allure-results'
-                      }],
-            disabled: false,
-            reportBuildPolicy: ALWAYS
+            includeProperties: false,
+            jdk: '',
+            properties: [],
+            reportBuildPolicy: 'ALWAYS',
+            results: [[path: 'build/reports/allure-results']]
         ])
       }
       stage('Publish notification in telegram') {
